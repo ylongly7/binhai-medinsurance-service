a5783a4a5f8f12789b30e5ed34ed3e07654cec85
gongxiaoyi <orzyaaaah@163.com>
2022-09-28 17:36:22 +0800

    服务点详情页面；gallery；引入百度地图

--- src/js/common.js
+++ src/js/common.js
@@ -42,3 +42,11 @@ String.prototype.format = function() {
     return formatted;
 };
  
+
+function getUrlParam(name)
+{
+  var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+  var r = window.location.search.substr(1).match(reg); //匹配目标参数
+  if (r!=null) return unescape(r[2]);
+  return null; //返回参数值
+}
---
+++ src/js/simple_gallery.js
@@ -0,0 +1,15 @@
+let initGallery = function(id,imgs){
+    let arr =imgs
+    console.log(arr)
+    let intv = 3000
+    setInterval(() => {
+        let a1 = arr.shift()
+        arr.push(a1)
+        //console.log(a1)
+        
+        $("#"+id).fadeTo(200,0.7,()=>{
+            $("#"+id).attr("src",a1)
+            $("#"+id).fadeTo(200,1)
+        })
+    }, intv);
+}
