781f648ba2080f9c1aed57a8d861a7f41160c7b3
gongxiaoyi <orzyaaaah@163.com>
2022-10-12 16:41:57 +0800

    修改样式

--- src/js/simple_gallery.js
+++ src/js/simple_gallery.js
@@ -7,7 +7,7 @@ let initGallery = function(id,imgs){
         arr.push(a1)
         //console.log(a1)
         
-        $("#"+id).fadeTo(200,0.7,()=>{
+        $("#"+id).fadeTo(200,0.9,()=>{
             $("#"+id).attr("src",a1)
             $("#"+id).fadeTo(200,1)
         })
