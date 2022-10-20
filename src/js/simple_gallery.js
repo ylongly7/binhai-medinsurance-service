de11ff11f3a83eaa42584c3843e1ffa7f4994d3d
gongxiaoyi <orzyaaaah@163.com>
2022-10-20 09:33:21 +0800

    更新图片，更新盐城医保url

--- src/js/simple_gallery.js
+++ src/js/simple_gallery.js
@@ -1,7 +1,16 @@
 var initGallery = function(id,imgs){
     var arr =imgs
     console.log(arr)
-    var intv = 3000
+    var intv = 4000
+    try{
+        $("#"+id).attr("src",arr[0])
+        var a1 = arr.shift()
+        arr.push(a1)
+    }
+    catch(e){
+
+    }
+    
     setInterval(() => {
         var a1 = arr.shift()
         arr.push(a1)
